d6d41769a619dbcce34669d768a0a03560e7a93e
DUXINLU <duxinlu81@live.com>
2020-01-19 14:33:18 +0800

    .

--- 53.py
+++ 53.py
@@ -2,7 +2,7 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        dp=[nums[0],]
-        for i in range(1,len(nums)):
-            dp.append(max(nums[i]+dp[i-1],nums[i]))
+        dp = [nums[0], ]
+        for i in range(1, len(nums)):
+            dp.append(max(nums[i] + dp[i - 1], nums[i]))
         return max(dp)
