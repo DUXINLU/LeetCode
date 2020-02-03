e5c4a363c2ce27075c3585326b080a52973efc9c
DUXINLU <duxinlu81@live.com>
2020-02-03 23:33:33 +0800

    .

--- 747.py
+++ 747.py
@@ -2,12 +2,12 @@ class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
         if not nums:
             return -1
-        if len(nums)==1:
+        if len(nums) == 1:
             return 0
-        max1=max(nums)
-        index_max1=nums.index(max1)
+        max1 = max(nums)
+        index_max1 = nums.index(max1)
         nums.pop(index_max1)
-        max2=max(nums)
-        if max1>=max2*2:
+        max2 = max(nums)
+        if max1 >= max2 * 2:
             return index_max1
         return -1
