3e6b78b8e97edfa4c819bc5ae865254b19aeebdf
DUXINLU <duxinlu81@live.com>
2020-03-15 20:11:02 +0800

    '.'

--- 3.py
+++ 3.py
@@ -1,3 +1,6 @@
+# coding=UTF-8
+'''
+version 1
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         res=0
@@ -13,3 +16,26 @@ class Solution:
             if _>res:
                 res=_
         return res
+'''
+'''
+version 2
+'''
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        slide = []
+        max_len = 0
+        for i in range(len(s)):
+            if s[i] not in slide:
+                slide.append(s[i])
+                if len(slide) > max_len:
+                    max_len = len(slide)
+            else:
+                slide[:] = slide[slide.index(s[i])+1:]
+                slide.append(s[i])
+        return max_len
+
+
+s = Solution()
+print(s.lengthOfLongestSubstring("pwwkew"))
